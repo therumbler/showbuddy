682ae506c88129426655b733cd0849b645841141
Benjamin Rumble <therumbler@gmail.com>
2024-06-28 15:25:34 -0400

    call ShowBuddy in web app

--- web.py
+++ web.py
@@ -5,13 +5,14 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
+from showbuddy import ShowBuddy
 
 logger = logging.getLogger(__name__)
 
 
 def make_app():
     logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
-
+    showbuddy = ShowBuddy()
     app = FastAPI()
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -22,12 +23,13 @@ def make_app():
     @app.get("/app")
     async def app_static():
         return FileResponse("static/app.html")
-        # return FivfvdfghjleResponse("static/dropover_tmp.html")
+        # return FileResponse("static/dropover_tmp.html")
 
     @app.post("/api/process")
     async def process(image: UploadFile = File(...), audio: UploadFile = File(...)):
         logger.info("image: %s", image.filename)
         logger.info("audio: %s", audio.filename)
-        return {"status": "ok"}
+        resp = showbuddy.process(audio, [image], audio.filename)
+        return resp
 
     return app
