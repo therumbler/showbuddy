313e13237ec00950b9c4000b6611e02d0b1dd4e7
Benjamin Rumble <therumbler@gmail.com>
2024-08-23 15:12:53 -0400

    delete all python code

--- entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec uvicorn --factory web:make_app --host 0.0.0.0 --port 5019
--- static/js/utils.js
+++
@@ -1,5 +0,0 @@
-function logToConsole(message, data) {
-    const c = document.getElementById('console');
-    c.innerHTML += `<p>${message} ${JSON.stringify(data)}</p>`;
-    console.log(message, data);
-}
