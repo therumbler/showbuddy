aa16a5696d01fe6c8c9d5ac20e3f855741da9734
Benjamin Rumble <therumbler@gmail.com>
2024-08-22 23:27:11 -0400

    use API keys from environment variables in the OpenAI tests

--- tests/integration/test_openai.py
+++ tests/integration/test_openai.py
@@ -1,18 +1,23 @@
+"""Async integration tests for the OpenAI API"""
+
+import os
 from unittest import IsolatedAsyncioTestCase
-from uuid import uuid4
 
 from lib.openai import OpenAI
 
 
 class TestOpenAI(IsolatedAsyncioTestCase):
+    """Async integration tests for the OpenAI API"""
+
     async def asyncSetUp(self):
         self._openai = OpenAI(
-            organization="organization",
-            project="project",
-            api_key="api_key",
+            organization=os.environ["OPENAI_ORGANIZATION_ID"],
+            project=os.environ["OPENAI_PROJECT_ID"],
+            api_key=os.environ["OPENAI_API_KEY"],
         )
 
     async def test_openai(self):
+        """test a simple completion"""
         content = "Hello, how are you?"
         expected_response = "I'm good, how are you?"
         resp = await self._openai.fetch_completions(content)
